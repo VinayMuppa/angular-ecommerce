cec4095ec49e4b118e628433fe0c091721f7b2ed
vinaybabumuppa <muppavinay@gmail.com>
2023-09-19 11:19:18 -0500

    added-code-for-viewing-cart

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -118,8 +118,7 @@ export class ProductListComponent implements OnInit {
     console.log(`Adding to cart: ${theProduct.name}, ${theProduct.unitPrice}`);
 
   
-    const theCartItem = new CartItem(theProduct.id!, theProduct.name!, theProduct.imageUrl!, theProduct.unitPrice!);
-
+    const theCartItem = new CartItem(theProduct);
     this.cartService.addToCart(theCartItem);
   }
 }
